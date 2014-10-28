04b06b668cbe96a7a90a7a9f26318f851d553456
Jo De Boeck <deboeck.jo@gmail.com>
2014-10-28 12:53:48 +0200

    Add autostart to jsagent

--- jpackages/jsagent/1.0/actions/process.configure.py
+++ jpackages/jsagent/1.0/actions/process.configure.py
@@ -2,5 +2,10 @@ def main(j,jp):
     #configure the application to autostart
     # jp.log("set autostart $(jp.name)")
 
-    #e.g. default:2,hypervisor:1,io:1
-    pass
+    cmd = 'python jsagent.py'
+    args = '--instance=%s' % jp.instance
+    workingdir = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'jsagent')
+    name="jsagent"
+    startstoptimeout=20
+    j.tools.startupmanager.addProcess(name, cmd, args=args, env={}, numprocesses=1, priority=100, shell=False, workingdir=workingdir,jpackage=jp,domain="",\
+        ports=[4445],check=True,timeoutcheck=startstoptimeout,stats=True,upstart=False)
